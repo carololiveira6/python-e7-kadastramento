48b1488853054f74becffe96d501907878c6723d
Carolina Oliveira <carolinasao6@gmail.com>
2021-05-19 17:55:40 -0300

    prototype

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/signup')
+def signup():
+    pass
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/profile/<int:user_id>')
+def update_user(user_id):
+    pass
+
+@app.route('/profile/<int:user_id>')
+def delete_user(user_id):
+    pass
+
+@app.route('/users')
+def all_users():
+    pass
